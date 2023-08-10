552793e76a40bd0b18e1ead964d3b0756724d48e
Jaufré Lallement <lallement.j.pro@outlook.fr>
2023-08-10 17:56:35 +0200

    Edit: finished implementing access retreiving from config

--- Maat/private/maat-process/config-process/Get-AccessFromConfig.ps1
+++ Maat/private/maat-process/config-process/Get-AccessFromConfig.ps1
@@ -7,31 +7,28 @@ function Get-AccessFromConfig {
       ValueFromPipelineByPropertyName = $false
     )]
     [ValidateNotNullOrEmpty()]
-    [MaatDirectory]  $Dir
+    $Dir
   )
 
   $dirName = $dir.GetName()
 
   # Retreive every mention of the specified directory
   $dirAccessGroups = $dir.GetAccessGroups()
-  Write-Host "`n$($dirAccessGroups.count) groups give access to $dirName :"
+  Write-Host "`n$($dirAccessGroups.count) groups give access to '$dirName' :"
+
   foreach ($maatAccessGroup in $dirAccessGroups) {
     Write-Host "$($maatAccessGroup.GetName()): $($maatAccessGroup.GetPermissions())"
 
     # Get the group node related to the directory to retreive its name
-    $accessGroupsInDomain = $adGroups.Where({ $_.Name -eq $configGroup.GetName() })
+    $accessGroupsInDomain = $adGroups.Where({ $_.Name -eq $maatAccessGroup.GetName() })
 
     foreach ($adAccessGroup in $accessGroupsInDomain) {
       Get-AccessRelatedUsers $adAccessGroup $maatAccessGroup
     }
   }
 
-  # Access feedback
-  $usersWithAccessToCurrentDir = $dir.GetAccessUsers()
-  Write-Host "`n$($usersWithAccessToCurrentDir.count) user have access to $dirName :"
-  foreach ($usr in $usersWithAccessToCurrentDir) {
-    $usrPermissions = $usr.GetDirPermissions($dir)
-    Write-Host "$($usr.GetSAN()): $usrPermissions"
-  }
+  Write-Host "Coucou"
 
+  # Access feedback
+  $dir.GetAccessFeedback()
 }
