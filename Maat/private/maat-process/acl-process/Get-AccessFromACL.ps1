224027fab9b153a12d8f38be3b8e088ffd0af0b2
Jaufré Lallement <lallement.j.pro@outlook.fr>
2023-09-03 19:14:24 +0200

    Edit: moved code from useless file separation

--- Maat/private/maat-process/acl-process/Get-AccessFromACL.ps1
+++ Maat/private/maat-process/acl-process/Get-AccessFromACL.ps1
@@ -10,22 +10,24 @@ function Get-AccessFromACL {
     $Dir
   )
 
-  $acl = Get-ACL -Path $dir.GetPath()
-
-  # $builtInAccess = $acl.Access.Where({ ($_.IdentityReference -like "*NT*\SYST*") -and ($_.IdentityReference -like "BUILTIN\Admin*") })
-  $nonBuiltInAccesses = $acl.Access.Where({ ($_.IdentityReference -notlike "*NT*\SYST*") -and ($_.IdentityReference -notlike "BUILTIN\*") })
-  Write-Host "`n$($nonBuiltInAccesses.count) groups give access to '$($dir.GetName())' :"
-
   # 1 acl access = 1 identity reference
-  foreach ($aclAccess in $nonBuiltInAccesses) {
+  foreach ($aclAccess in $dir.GetNonBuiltInACLAccesses()) {
     # Translate MS file system rights to simple R/RW short string
-    $accessPermissions = Format-ACLAccessRights $aclAccess.FileSystemRights
+    $accessPermissions = ""
+    if ($aclAccess.FileSystemRights -like "*Read*") {
+      $accessPermissions = "R"
+    }
+
+    if ($AccessRights -like "*Modify*") {
+      $accessPermissions = "RW"
+    }
+
     $adACLGroups = Get-ADGroupFromACL -IdentityReference $aclAccess.IdentityReference
 
     # Create access group instance + bind it to the directory
-    [MaatAccess]$maatAccessToDir = [MaatAccess]::new($dir, $accessPermissions)
+    [MaatAccess]$maatAccessToDir = [MaatAccess]::new($dir, $accessPermissions, "acl")
     $maatAccessGroup = $dir.GetResultRef().GetUniqueAccessGroup($adACLGroups[0].Name, $maatAccessToDir)
-    $dir.AddACLGroup($maatAccessGroup)
+    $dir.AddAccessGroup($maatAccessGroup)
 
     # Get AD Groups based on name of the retreived group with identity reference
     # If identity reference is an sid, it is linked to only one domain. But multiple domains can be provided
